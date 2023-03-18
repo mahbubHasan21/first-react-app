ce6aa21092ec13969f2097f7176da5a8934d3881
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2023-03-18 22:41:57 +0600

    0.1

--- src/App.js
+++ src/App.js
@@ -20,17 +20,38 @@ class App extends Component {
     });
   };
 
+  changeWithInputState = (event, index) => {
+    const book = {
+      ...this.state.books[index],
+    };
+    book.bookName = event.target.value;
+
+    const books = [...this.state.books];
+    books[index] = book;
+    this.setState({
+      books: books,
+    });
+  };
+
   render() {
-    return this.state.books.map((book, index) => {
+    const books = this.state.books.map((book, index) => {
       return (
         <Book
           bookName={book.bookName}
           writer={book.writer}
           delete={() => this.deleteBookState(index)}
+          inputName={(event) => this.changeWithInputState(event, index)}
           key={index}
         />
       );
     });
+
+    return (
+      <div className="app">
+        <h2>Book list</h2>
+        {books};
+      </div>
+    );
   }
 }
 
